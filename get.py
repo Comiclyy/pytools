6a9ba86288bfbfc6582f46970b17764688fd2908
Comicly <93946611+Comicly69@users.noreply.github.com>
2023-02-10 17:58:56 +0000

    fix errors and add features

--- get.py
+++ get.py
@@ -15,19 +15,35 @@ while True:
     else:
         url = input("Enter the URL you want to send a GET request to: ")
 
+        if not url.startswith("https://"):
+            url = "https://" + url
+            
     num_requests = int(input("Enter the number of requests you want to send: "))
     num_threads = int(input("Enter the number of threads you want to use: "))
 
+    use_random_data = input("Use random data? (yes/no)")
+    if use_random_data.lower() == 'yes':
+        data_length = int(input("Enter the number of characters for the random data: "))
+    else:
+        data = input("Enter the data you want to send: ")
+    
     def send_request(idx):
-        data = 'apple' + ''.join(random.choices(string.ascii_letters + string.digits, k=10))
+        if use_random_data.lower() == 'yes':
+            data = 'comicly' + ''.join(random.choices(string.ascii_letters + string.digits, k=data_length))
+        else:
+            data = data
         payload = {"data": data}
         headers = {}
 
         response = requests.request("GET", url, headers=headers, params=payload)
-        with open('responses.txt', 'a') as file:
-            file.write(str(response.json()))
-        print(f"Request {idx} status: {response.status_code} {data}")
-        return response.json()
+        try:
+            response_json = response.json()
+        except ValueError as e:
+            response_json = {}
+            print(f"Request {idx} error: {e}")
+
+        print(f"Request {idx} status: {response.status_code} {data} sent to {url}")
+        return response_json
 
     threads = []
     for i in range(num_requests):
