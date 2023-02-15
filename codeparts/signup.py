6f30919136fb78002c3de23cac21da9856ace36f
Comicly <93946611+Comicly69@users.noreply.github.com>
2023-02-14 19:10:16 -0500

    reorganise

---
+++ codeparts/signup.py
@@ -0,0 +1,29 @@
+import requests
+import random
+import string
+
+url = 'https://db264493-57ad-4786-8ad2-c8d35c85ddea.mock.pstmn.io'
+
+username = ''.join(random.choices(string.ascii_letters + string.digits, k=10))
+email = f"{''.join(random.choices(string.ascii_letters + string.digits, k=8))}@dispostable.com"
+password = ''.join(random.choices(string.ascii_letters + string.digits, k=15))
+
+data = {
+    'username': username,
+    'email': email,
+    'password': password
+}
+
+response = requests.post(url, data=data)
+
+if response.status_code == 200:
+    print('Successfully signed up!')
+else:
+    print('Sign up failed with status code:', response.status_code)
+
+with open('signupdata.txt', 'w') as f:
+    f.write(f"website_url= {url}\n")
+    f.write(f"request status= {response.status_code}\n")
+    f.write(f"username= {username}\n")
+    f.write(f"email= {email}\n")
+    f.write(f"password= {password}\n")
---
+++ main.bat
@@ -0,0 +1,26 @@
+@echo off
+:begin
+echo Hello, please choose a program to continue
+echo.
+echo getpy: Python program to run get requests with many parameters
+echo postpy: Python program to run post requests with many parameters
+echo signuppy: Python program to run different types of requests with many lines of request headers (In development)
+set /p startpro=
+
+if "%startpro%" == "getpy" (
+    echo starting get.py...
+    start C:\Users\comic\Documents\GitHub\requestspammer\codeparts\get.py
+    goto begin
+)
+
+if "%startpro%" == "postpy" (
+    echo starting post.py...
+    start C:\Users\comic\Documents\GitHub\requestspammer\codeparts\post.py
+    goto begin
+)
+
+if "%startpro%" == "signuppy" (
+    echo starting signup.py...
+    start C:\Users\comic\Documents\GitHub\requestspammer\codeparts\signup.py
+    goto begin
+)
