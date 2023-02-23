a86622c5cac762c149eeadfad602e7f9c907005c
Comicly <93946611+Comicly69@users.noreply.github.com>
2023-02-23 15:32:28 +0000

    add writing func

--- codeparts/getcookie.py
+++ codeparts/getcookie.py
@@ -1,21 +1,32 @@
+import json
 import random
 import string
 import hashlib
+import platform
+import uuid
+import socket
+
+# Get the HWID
+hwid = str(uuid.getnode())
+
+# Get the IP address
+ip = socket.gethostbyname(socket.gethostname())
 
 while True:
     def algorithm():
         print("Baking cookie and putting it in the oven...")
-        unhashed_cookie = 'c' + ''.join(random.choices(string.ascii_letters + string.digits, k=31))
+        unhashed_cookie = 'c' + hwid + ip + ''.join(random.choices(string.ascii_letters + string.digits, k=21))
         hash_object = hashlib.sha256(unhashed_cookie.encode())
-        hashedcookie_str = hash_object.hexdigest()
-        hashed_cookie = hashedcookie_str
+        hashed_cookie = hash_object.hexdigest()
         print("Cookie finished baking!")
-        return hashed_cookie
+        return hashed_cookie, unhashed_cookie
 
-    def getcookie(hashed_cookie):
-        print("Your cookie is", hashed_cookie)
+    def getcookie(hashed_cookie, unhashed_cookie):
+        with open('/workspaces/requestspammer/data/cookie.json', 'w') as f:
+            f.write(json.dumps({'hashed_cookie': hashed_cookie}) + '\n' + json.dumps({'unhashed_cookie': unhashed_cookie}))
+        print("Cookie saved to /workspaces/requestspammer/data/cookie.json")
 
-    hashed_cookie = algorithm()
-    getcookie(hashed_cookie)
+    hashed_cookie, unhashed_cookie = algorithm()
+    getcookie(hashed_cookie, unhashed_cookie)
 
     input("Press enter to run the program again, otherwise close the window.\n")
